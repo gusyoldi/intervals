2feebbd08c805cbbff0629eb0e87ea8fc8bc79bf
Gustavo Yoldi <gyoldi@auronix.com>
2024-05-19 11:36:19 -0300

    cambia el theme del proyecto y agrega un modo dark

--- app/adivinar-intervalo/page.tsx
+++ app/adivinar-intervalo/page.tsx
@@ -1,14 +1,13 @@
-import Display from "@/components/ui/Display/Display";
-import Keyboard from "@/components/ui/Keyboard/Keyboard";
-import { INTERVALOS } from "@/lib/data/db";
+import Display from "@/app/adivinar-intervalo/components/Display/Display";
+import Keyboard from "@/app/adivinar-intervalo/components/Keyboard/Keyboard";
 
-export default async function Page() {
+export default async function AdivinarIntervaloPage() {
   return (
-    <div className="flex flex-col justify-between p-12 gap-6 bg-slate-100">
-      <h1 className="text-center text-red-500 text-2xl">Que intervalo es?</h1>
-
+    <div className="flex flex-col justify-between p-12 gap-6 ">
+      <h1 className="text-center text-red-500 text-2xl underline">
+        Que intervalo es?
+      </h1>
       <Display />
-
       <Keyboard />
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/ui/ui/button/button";
 import Link from "next/link";
 
 export default async function HomePage() {
   return (
-    <ul className="flex-col justify-center items-center gap-4 bg-slate-300 p-4 rounded-md shadow-md max-w-60 m-auto mt-10">
+    <ul className="flex-col justify-center items-center gap-4 p-4 rounded-md shadow-md max-w-60 m-auto mt-10">
       <li className="text-center">
         <Button variant="default">
           <Link href="/adivinar-intervalo">Adivinar intervalo</Link>
